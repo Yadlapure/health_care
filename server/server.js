3b977d290278df95089026d9f6ead15a5ba9e3e4
SachinYadlapure <sachinyadlapure5@gmail.com>
2025-05-15 11:18:17 +0530

    tweak

--- server/server.js
+++ server/server.js
@@ -10,12 +10,10 @@ app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("Mongo connected"))
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("MongoDB connected successfully"))
   .catch((err) => console.error("MongoDB connection error:", err));
+
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
